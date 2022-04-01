60519d21dfe2a7408361bcb7fa16cea7a9cdaeaf
jessicaly <jessiclyaruiz@hotmail.com>
2022-03-31 23:21:21 -0500

    literal 5

--- PruebaModulo4JY/src/main/java/ec/edu/uce/controller/CitaMedicaController.java
+++ PruebaModulo4JY/src/main/java/ec/edu/uce/controller/CitaMedicaController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ec.edu.uce.repository.modelo.CitaMedica;
+
 import ec.edu.uce.service.ICitaMedicaService;
 
 @Controller
@@ -34,5 +39,24 @@ public class CitaMedicaController {
 
 		}
 
+		
+		//primer metodo para actualizar
+		
+		@GetMapping("citaActualiza/{numCita}")
+		public String  obtenerPaginaActualizarCita(@PathVariable("numCita") String numCita, CitaMedica cita, Model modelo) {
+			CitaMedica citaMedica=this.citaService.buscarNumero(numCita);
+			modelo.addAttribute("cita", citaMedica);
+			return "citaActualiza";
+			
+		}
+		
+		@PutMapping("actualizar/{numCita}")
+		public String actalizarCita(@PathVariable("numCita") String numCita, CitaMedica citaMedica, BindingResult result, Model modelo, RedirectAttributes redirectAttributes) {
+			citaMedica.setNumero(numCita);
+			this.citaService.update(citaMedica);
+			return "actualizaMsj";
+		}
+		
+		
 	
 }
